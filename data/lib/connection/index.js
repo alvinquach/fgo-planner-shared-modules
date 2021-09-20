016afab1f5ea06c4921894a32e291963d7b94dc8
Alvin Quach <alvinquach91@gmail.com>
2021-09-19 19:07:30 -0700

    added function to initialize mongoose connection

--- data/lib/connection/index.js
+++ data/lib/connection/index.js
@@ -10,8 +10,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./connection"), exports);
-__exportStar(require("./models"), exports);
-__exportStar(require("./schemas"), exports);
-__exportStar(require("./types"), exports);
-__exportStar(require("./validators"), exports);
+__exportStar(require("./mongoose-connection"), exports);
--- data/lib/index.d.ts
+++ data/lib/index.d.ts
@@ -1,3 +1,4 @@
+export * from './connection';
 export * from './models';
 export * from './schemas';
 export * from './types';
